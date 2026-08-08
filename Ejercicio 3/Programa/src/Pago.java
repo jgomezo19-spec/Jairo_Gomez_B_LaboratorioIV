8228178bee8732eb62b35470401d8c0a305f4076
jgomezo19-spec <jgomezo19@miumg.edu.gt>
2026-08-07 19:31:25 -0600

    Ejercicio 3

---
+++ Ejercicio 3/Programa/src/Pago.java
@@ -0,0 +1,12 @@
+public class Pago {
+
+    protected double monto;
+
+    public Pago(double monto) {
+        this.monto = monto;
+    }
+
+    public void procesarPago() {
+        System.out.println("Procesando pago de: Q" + monto);
+    }
+}
